88e490bd92f35a257050bb8658dcd74e9db99b66
Zachariah Schmunk <z-schmunk@onu.edu>
2024-11-07 00:28:22 -0500

    even indices

--- Lab 9.1/Lab 9.1.cpp
+++ Lab 9.1/Lab 9.1.cpp
@@ -9,12 +9,20 @@ int main() {
     srand(static_cast<unsigned int>(time(0)));
 
     for (int i = 0; i < SIZE; ++i) {
-        arr[i] = rand() % 9000 + 1000; // Generate 4-digit random integers
+        arr[i] = rand() % 9000 + 1000;
     }
 
+    cout << "The random integers:";
     for (int i = 0; i < SIZE; ++i) {
-        cout << arr[i] << endl; // Print each element
+        cout << arr[i] << endl;
     }
+    cout << endl;
+
+    cout << "Elements at even indices:\n";
+    for (int i = 0; i < 10; i += 2) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
